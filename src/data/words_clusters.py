706a6388d1112f46010434a25771f86cea06f832
Jacopo Boscariol <boscarioljacopo@gmail.com>
2024-11-11 13:40:05 +0100

    src/data update on words_clusters

---
+++ src/data/words_clusters.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+# space cluster
+cluster_space = [
+    "spaceship",
+    "space station",
+    "rocket",
+    "shuttle",
+    "space suit",
+    "planet",
+    "moon",
+    "asteroid",
+    "comet",
+    "black hole",
+    "wormhole",
+    "nebula",
+    "supernova",
+    "galaxy",
+    "discovery",
+    "spacewalk",
+    "alien",
+    "astronaut",
+    "scientist",
+    "engineer",
+    "pilot",
+    "commander",
+    "exploration",
+]
+
+communism_cluster = [
+    "communism", "Marxism", "Leninism", "Stalinism", "socialism",
+    "proletariat", "revolution", "Red Army", "Soviet Union", "USSR", "Marx", "Lenin", "Trotsky", "Stalin", "Mao", "Karl Marx",
+    "dictatorship of the proletariat", "state socialism", "planned economy", 
+    "collectivism", "worker's state", "egalitarianism", "classless society", 
+    "collective ownership", "centralization", "Soviet", "Five-Year Plan", 
+    "Great Leap Forward", "Cultural Revolution", "Communist Party", "red flag",
+    "Comintern", "Che Guevara", "Revolutionary Guard", "collective farm", 
+    "glasnost", "perestroika", "East Germany", "Berlin Wall", "Cuban Revolution", 
+    "Vietnam War", "Cold War", "Marxist-Leninist", "workers' rights", "social equality",
+    "imperialism", "anti-imperialism", "anti-capitalism", "proletarian internationalism",
+    "Red October", "bourgeois dictatorship", "Khrushchev", "Gorbachev", "Trotskyism"
+]
