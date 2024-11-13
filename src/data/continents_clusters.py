a60b9739ad42de2162b92bbbe6ea95da426cfbdd
Jacopo Boscariol <boscarioljacopo@gmail.com>
2024-11-13 16:03:16 +0100

    adding continents_clusters.py over-passing .gitignore

---
+++ src/data/continents_clusters.py
@@ -0,0 +1,42 @@
+north_america = [
+    'United States of America', 'Canada', 'Mexico', 'Cuba', 'Puerto Rico', 'Panama',
+    'Jamaica', 'Costa Rica', 'Bahamas', 'Haiti'
+]
+
+europe = [
+    'United Kingdom', 'France', 'Italy', 'Germany', 'Spain', 'Netherlands', 'Sweden',
+    'West Germany', 'Denmark', 'Czechoslovakia', 'Poland', 'Belgium', 'Norway', 'Hungary',
+    'German Democratic Republic', 'Yugoslavia', 'Ireland', 'Switzerland', 'Austria',
+    'Czech Republic', 'Finland', 'England', 'Greece', 'Portugal', 'Croatia', 'Romania',
+    'Bulgaria', 'Iceland', 'Luxembourg', 'Weimar Republic', 'Serbia', 'Scotland', 'Estonia',
+    'Bosnia and Herzegovina', 'Slovakia', 'Slovenia', 'Albania', 'Ukraine', 'Nazi Germany',
+    'Republic of Macedonia', 'Lithuania', 'Serbia and Montenegro', 'Socialist Federal Republic of Yugoslavia',
+    'Kingdom of Great Britain', 'Federal Republic of Yugoslavia', 'Slovak Republic', 'Wales',
+    'Monaco', 'Cyprus', 'Malta', 'Northern Ireland', 'Montenegro', 'Ukrainian SSR', 'Isle of Man',
+    'Kingdom of Italy', 'Crimea', 'German Language', 'Ukrainian SSR'
+]
+
+asia = [
+    'India', 'Japan', 'Hong Kong', 'South Korea', 'China', 'Soviet Union', 'Philippines',
+    'Russia', 'Israel', 'Turkey', 'Thailand', 'Taiwan', 'Iran', 'Pakistan', 'Indonesia',
+    'Malaysia', 'Sri Lanka', 'Singapore', 'Bangladesh', 'Lebanon', 'Nepal', 'Vietnam',
+    'Korea', 'Azerbaijan', 'Georgia', 'Iraq', 'United Arab Emirates', 'Afghanistan', 'Armenia',
+    'Palestinian territories', 'Georgian SSR', 'Mandatory Palestine', 'Uzbek SSR', 'Uzbekistan',
+    'Mongolia', 'Kuwait', 'Bahrain', 'Bhutan', 'Qatar', 'Soviet occupation zone', 'Malayalam Language',
+    'Jordan', 'Iraqi Kurdistan', 'Republic of China', 'Macau', 'Palestinian Territories'
+]
+
+australia = [
+    'Australia', 'New Zealand'
+]
+
+south_america = [
+    'Argentina', 'Brazil', 'Chile', 'Colombia', 'Venezuela', 'Peru', 'Uruguay',
+    'Bolivia', 'Guinea', 'Aruba'
+]
+
+africa = [
+    'South Africa', 'Egypt', 'Cambodia', 'Morocco', 'Senegal', 'Burkina Faso', 'Algeria',
+    'Tunisia', 'Burma', 'Nigeria', 'Republic Democratic of the Congo', 'Mali', 'Kenya', 'Cameroon',
+    'Ethiopia', 'Zimbabwe', 'Congo', 'Libya', 'Zambia', 'Guinea-Bissau'
+]
