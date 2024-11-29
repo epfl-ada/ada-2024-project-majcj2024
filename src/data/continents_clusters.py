0c93a55c99772e082a48857c7ece343a7e66336b
jake pringle <jakepringle@Jakes-MacBook-Pro.local>
2024-11-30 00:32:42 +0100

    new regions and ratings

--- src/data/continents_clusters.py
+++ src/data/continents_clusters.py
@@ -1,50 +1,64 @@
-north_america = [
-    'United States of America', 'Canada', 'Mexico', 'Cuba', 'Puerto Rico', 'Panama',
-    'Jamaica', 'Costa Rica', 'Bahamas', 'Haiti'
+na = [
+    'United States of America', 'Canada'
 ]
 
-europe = [
-    'United Kingdom', 'France', 'Italy', 'Germany', 'Spain', 'Netherlands', 'Sweden',
-    'West Germany', 'Denmark', 'Czechoslovakia', 'Poland', 'Belgium', 'Norway', 'Hungary',
-    'German Democratic Republic', 'Yugoslavia', 'Ireland', 'Switzerland', 'Austria',
-    'Czech Republic', 'Finland', 'England', 'Greece', 'Portugal', 'Croatia', 'Romania',
-    'Bulgaria', 'Iceland', 'Luxembourg', 'Weimar Republic', 'Serbia', 'Scotland', 'Estonia',
-    'Bosnia and Herzegovina', 'Slovakia', 'Slovenia', 'Albania', 'Ukraine', 'Nazi Germany',
-    'Republic of Macedonia', 'Lithuania', 'Serbia and Montenegro', 'Socialist Federal Republic of Yugoslavia',
-    'Kingdom of Great Britain', 'Federal Republic of Yugoslavia', 'Slovak Republic', 'Wales',
-    'Monaco', 'Cyprus', 'Malta', 'Northern Ireland', 'Montenegro', 'Ukrainian SSR', 'Isle of Man',
-    'Kingdom of Italy', 'Crimea', 'German Language', 'Ukrainian SSR'
+sa_and_ca = [
+    'Mexico', 'Cuba', 'Puerto Rico', 'Panama', 'Jamaica', 'Costa Rica', 'Bahamas', 'Haiti',
+    'Argentina', 'Brazil', 'Chile', 'Colombia', 'Venezuela', 'Peru', 'Uruguay', 'Bolivia', 'Aruba'
 ]
 
-asia = [
-    'India', 'Japan', 'Hong Kong', 'South Korea', 'China', 'Soviet Union', 'Philippines',
-    'Russia', 'Israel', 'Turkey', 'Thailand', 'Taiwan', 'Iran', 'Pakistan', 'Indonesia',
-    'Malaysia', 'Sri Lanka', 'Singapore', 'Bangladesh', 'Lebanon', 'Nepal', 'Vietnam',
-    'Korea', 'Azerbaijan', 'Georgia', 'Iraq', 'United Arab Emirates', 'Afghanistan', 'Armenia',
-    'Palestinian territories', 'Georgian SSR', 'Mandatory Palestine', 'Uzbek SSR', 'Uzbekistan',
-    'Mongolia', 'Kuwait', 'Bahrain', 'Bhutan', 'Qatar', 'Soviet occupation zone', 'Malayalam Language',
-    'Jordan', 'Iraqi Kurdistan', 'Republic of China', 'Macau', 'Palestinian Territories'
+western_europe = [
+    'United Kingdom', 'France', 'Italy', 'Germany', 'Spain', 'Netherlands', 'Sweden', 
+    'West Germany', 'Denmark', 'Belgium', 'Norway', 'Ireland', 'Switzerland', 'Austria', 
+    'Czech Republic', 'Finland', 'England', 'Greece', 'Portugal', 'Iceland', 'Luxembourg', 
+    'Scotland', 'Wales', 'Monaco', 'Malta', 'Northern Ireland', 'Isle of Man', 
+    'Kingdom of Great Britain', 'Weimar Republic'
+]
+
+eastern_europe = [
+    'Czechoslovakia', 'Poland', 'Hungary', 'German Democratic Republic', 'Yugoslavia', 
+    'Croatia', 'Romania', 'Bulgaria', 'Estonia', 'Bosnia and Herzegovina', 'Slovakia', 
+    'Slovenia', 'Albania', 'Ukraine', 'Republic of Macedonia', 'Lithuania', 
+    'Serbia', 'Serbia and Montenegro', 'Federal Republic of Yugoslavia', 
+    'Socialist Federal Republic of Yugoslavia', 'Slovak Republic', 'Montenegro', 
+    'Ukrainian SSR', 'Crimea', 'Georgian SSR', 'Uzbek SSR'
+]
+
+south_africa_and_central_africa = [
+    'South Africa', 'Nigeria', 'Democratic Republic of the Congo', 'Mali', 'Kenya', 
+    'Cameroon', 'Ethiopia', 'Zimbabwe', 'Congo', 'Zambia', 'Guinea-Bissau', 
+    'Senegal', 'Burkina Faso', 'Guinea'
+]
+
+north_africa_and_middle_east = [
+    'Egypt', 'Morocco', 'Algeria', 'Tunisia', 'Libya', 'Iran', 'Iraq', 'Israel', 
+    'Turkey', 'Lebanon', 'Jordan', 'United Arab Emirates', 'Palestinian territories', 
+    'Palestinian Territories', 'Kuwait', 'Bahrain', 'Qatar', 'Afghanistan', 'Iraqi Kurdistan', 
+    'Mandatory Palestine'
+]
+
+india = [
+    'India'
+]
+
+remaining_asia = [
+    'Japan', 'Hong Kong', 'South Korea', 'Philippines', 'Thailand', 'Taiwan', 'Indonesia', 
+    'Malaysia', 'Sri Lanka', 'Singapore', 'Bangladesh', 'Nepal', 'Macau', 'Vietnam', 'Mongolia', 'Republic of China'
 ]
 
 oceania = [
     'Australia', 'New Zealand'
 ]
 
-south_america = [
-    'Argentina', 'Brazil', 'Chile', 'Colombia', 'Venezuela', 'Peru', 'Uruguay',
-    'Bolivia', 'Guinea', 'Aruba'
-]
-
-africa = [
-    'South Africa', 'Egypt', 'Cambodia', 'Morocco', 'Senegal', 'Burkina Faso', 'Algeria',
-    'Tunisia', 'Burma', 'Nigeria', 'Democratic Republic of the Congo', 'Mali', 'Kenya', 'Cameroon',
-    'Ethiopia', 'Zimbabwe', 'Congo', 'Libya', 'Zambia', 'Guinea-Bissau'
-]
-
 # defining continents dictionary
-continents = {'North America': north_america,
-              'South America': south_america,
-              'Europe': europe,
-              'Asia': asia,
-              'Oceania': oceania,
-              'Africa': africa}
+regions = {
+    'North America': na,
+    'Central and South America': sa_and_ca,
+    'Western Europe': western_europe,
+    'Eastern Europe': eastern_europe,
+    'South Africa and Central Africa': south_africa_and_central_africa,
+    'North Africa and Middle East': north_africa_and_middle_east,
+    'India': india,
+    'Remaining Asia': remaining_asia,
+    'Oceania': oceania
+}
