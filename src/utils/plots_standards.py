79e94e9f264905f1d2abc1387f1420f89eddb3a7
Jacopo Boscariol <boscarioljacopo@gmail.com>
2024-12-18 23:25:16 +0100

    added global styles dictionary in plots_standards.py

--- src/utils/plots_standards.py
+++ src/utils/plots_standards.py
@@ -1,6 +1,23 @@
-# standard histogram style
-hist_style = {
-    'edgecolor': 'black',
-    'alpha': 0.7,
-    'color': 'cornflowerblue'
+# Dictionary to store all styles
+styles = {
+    'hist_style': {
+        'edgecolor': 'black',
+        'alpha': 0.7,
+        'color': 'cornflowerblue'
+    },
+
+    'boxplot_style': {
+        'color': 'cornflowerblue',
+        'linecolor': 'black',
+        'width': 0.3
+    },
+
+    'scatter_style': {
+        's': 5
+    },
+
+    'line_style': {
+        'color': 'red'
+    },
+    
 }
