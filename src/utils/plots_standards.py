000c7db709051b32286fbd779b39dc14ea01f1eb
Jacopo Boscariol <boscarioljacopo@gmail.com>
2024-12-19 14:47:45 +0100

    plots_standards.py submodule updates

--- src/utils/plots_standards.py
+++ src/utils/plots_standards.py
@@ -23,3 +23,18 @@ styles = {
     },
     
 }
+
+# defining color palette for top 20 genres
+genres_colors = [
+    '#FF5733', '#FF8D1A', '#FFB300', '#FFEB3B', '#8BC34A', 
+    '#4CAF50', '#009688', '#00BCD4', '#2196F3', '#3F51B5', 
+    '#9C27B0', '#E91E63', '#F44336', '#FF5722', '#795548', 
+    '#607D8B', '#8D6E63', '#FF9800', '#9E9D24', '#43A047'
+]
+
+# defining color palette for regions
+regions_colors = [
+    '#003366', '#4B0082', '#FF7F00', '#FFD700',
+      '#FF5733', '#8A2BE2', '#00BFFF', '#C71585',
+        '#228B22'
+]
